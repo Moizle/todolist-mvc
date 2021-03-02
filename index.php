9f933b7d53e36c8aaf46e15ab351c5bf2f58d6cc
Moiz <m_zhara@mail.fhsu.edu>
2021-03-02 01:58:23 -0500

    Could only get this far. Buttons do not work. Stuck with index.php and view files.

--- index.php
+++ index.php
@@ -49,6 +49,89 @@
     <header>
         <h1>To Do List</h1>
     </header>
-   
+    <!-- <main>
+    <!-- <?php if(!$newtitle) {?>
+        <!-- Add a task to DB -->
+        <section>
+            <h2>Add a task</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+            <label for="newtitle">Title:</label>
+            <input type="text" autocomplete="off" id="newtitle" name="newtitle" maxlength="20" required>
+            <br>
+            <label for="newdesc">Description:</label>
+            <input type="text" autocomplete="off" id="newdesc" name="newdesc" maxlength="50" required>
+            <br>
+            <button type="submit" id="add">+ or press enter</button>
+            </form>
+        </section>
+        <section>
+            <div id="table">
+            <h2>Current Tasks</h2>
+            <!-- Check for tasks -->
+            <?php if (!$tasks)
+            {
+                echo "No to do list items exist yet.";
+            }
+            else
+            {
+                echo "Here's what is on the to do list.";
+            }
+            ?>
+            <br>
+            <table>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Done?</th>
+            </tr>
+            <!-- Display tasks -->
+            <?php foreach ($tasks as $task):?>
+            <tr>
+            <td><?php echo $task['Title'];?></td>
+            <td><?php echo $task['Description'];?></td>
+            <td><form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+            <?php require("database.php");
+                $id = filter_input(INPUT_POST,'id',FILTER_VALIDATE_INT);
+                // Delete Task
+                if ($id != false)
+                {
+                    $query = 'DELETE FROM todoitems
+                                WHERE ItemNum = :id';
+                    $statement= $db->prepare($query);
+                    $statement->bindValue(':id',$id);
+                    $success = $statement->execute();
+                    $statement->closeCursor();
+                    header("Location:.");
+                }
+                ?>
+                <input type="hidden" name="id"
+                    value="<?php $task['ItemNum'];?>">
+                <button type="submit" class="delete">✓</button></td>
+            </tr>
+            <?php endforeach; ?>
+            </table>
+        </div>
+        </section>
+
+        <?php } else {?>
+            <?php require("database.php"); ?>
+            <?php 
+                if ($newtitle)
+                {
+                    $query ='INSERT INTO todoitems
+                                (Title,Description)
+                            VALUES
+                                (:newtitle,:newdesc)';
+                    $statement = $db->prepare($query);
+                    $statement->bindValue(':newtitle', $newtitle);
+                    $statement->bindValue(':newdesc', $newdesc);
+                    $statement->execute();
+                    $statement->closeCursor();
+                    header("Location:.");
+                }
+            ?>
+
+        <?php }?> -->
+    </main> -->
 </body>
 </html>
